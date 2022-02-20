5e167c7261acaaa384d3bd08f1ad87f282385c25
deepak40k <deepak40.k@gmail.com>
2022-02-20 17:57:27 +0100

    dummy property added

--- src/main/java/com/casestudy/skilltracker/admin/Application.java
+++ src/main/java/com/casestudy/skilltracker/admin/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-/*@EnableMongoRepositories*/
 public class Application {
 
 	public static void main(String[] args) {
