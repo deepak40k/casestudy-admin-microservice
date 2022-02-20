d34d19b020531ba371040f8830b32a062452c8c8
deepak40k <deepak40.k@gmail.com>
2022-02-20 17:03:47 +0100

    appspec.yml change

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./bash_profile
 cd /home/ec2-user/app
+. /home/ec2-user/.bash_profile
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:amazon-cloudwatch-agent.json
 sudo nohup java -jar -Dspring.rabbitmq.addresses=$mqUri -Dspring.rabbitmq.username=$mqUserName -Dspring.rabbitmq.password=$mqPwd -Dspring.data.mongodb.uri=$mongoDBUri -Dspring.data.mongodb.database=admin -Damazon.aws.accessKey=$accessKey -Damazon.dynamodb.endpoint=$secretKey -Damazon.dynamodb.region=$region -Dmemcached.port=$memcachedPort -Dmemcached.configEndpoint=$memcachedEndpoint -Dserver.port=80 *.jar 2> /dev/null &
